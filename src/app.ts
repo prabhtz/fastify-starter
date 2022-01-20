da9f2bac9dc866c61197c30dee72e42525001d85
prabhtz <gunpaul41@gmail.com>
2022-01-20 19:54:46 +0545

    Add prefix to routes

--- src/app.ts
+++ src/app.ts
@@ -9,11 +9,14 @@ import { FastifyInstance } from 'fastify';
  * @param {any} opts
  * @returns {Promise<void>}
  */
-async function app(fastify: FastifyInstance, opts: any): Promise<void> {
+async function app(fastify: FastifyInstance, opts: any = {}): Promise<void> {
   // Load and register all the routes.
   fastify.register(autoload, {
     dir: join(__dirname, 'routes'),
-    options: opts
+    options: {
+      ...opts,
+      prefix: '/api'
+    }
   });
 }
 
