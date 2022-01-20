64c5674547b2bf2b3872060b9fe4aa0c946d4684
prabhtz <gunpaul41@gmail.com>
2022-01-20 19:55:24 +0545

    Send reply from controller

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -1,10 +1,12 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+
 import * as homeService from '../services/home';
 
 /**
  * Get application information.
  */
-export async function getAppInfo() {
+export async function getAppInfo(request: FastifyRequest, reply: FastifyReply) {
   const data = await homeService.getAppInfo();
 
-  return data;
+  reply.send(data);
 }
